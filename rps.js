f44e41f145ce564da48cb2a968816ab417f6aada
Jonniesam <117244803+Jonniesam@users.noreply.github.com>
2022-11-14 17:53:53 -0800

    Added scores and rounds, added if else statement  for overall winner of game function

--- rps.js
+++ rps.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let cpuScore = 0;
 
 //CPU randomly genterates a number 0-2 which corresponds to a choice of rock/paper/or scissors
 function getComputerChoice() {
@@ -15,17 +17,21 @@ function getComputerChoice() {
 
 // Compares users selection vs computer selection
 function playRound(getPlayerChoice, computerSelection) {
+    // When the user wins
     if(getPlayerChoice === 'rock' && computerSelection === 'scissors' || computerSelection === 'scissors' && computerSelection === 'paper' || getPlayerChoice === 'paper' && computerSelection === 'rock') {
+        playerScore++;
         return `User Wins! ${getPlayerChoice} beats ${computerSelection}!`
+    //if it's a tie game
     } else if(getPlayerChoice === computerSelection) { 
         return 'Tie game!'
+    //when the computer wins
     } else {
+        cpuScore++;
         return `Computer Wins! ${computerSelection} beats ${getPlayerChoice}!`
     }
 };
 
 //Returns results of round
-//console.log(playRound(getPlayerChoice, computerSelection));
 
 function game() {
     let round = 0
@@ -33,8 +39,18 @@ function game() {
     const computerSelection = getComputerChoice();
     const question = prompt("Choose 1: rock, paper, or scissors");
     const getPlayerChoice = question.toLowerCase();
+    round++;
+    console.log(`Round ${round}`);
     console.log(playRound(getPlayerChoice, computerSelection));
+    console.log(playerScore);
+    console.log(cpuScore);
+} if(playerScore > cpuScore){
+    return 'Humans win!'
+} else if(playerScore < cpuScore) {
+    return 'Computers win!'
+} else {
+    return `It's a tie!`
 }
 };
 
-game();
+console.log(game());
